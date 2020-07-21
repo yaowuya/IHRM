eb3277ddb98a6c7d4374db05c79ec5d6d19cc7d6
yaowuya <zrfyaowuya@126.com>
2020-07-21 19:12:19 +0800

    增删查改

--- ihrm-company/src/main/java/com/ihrm/company/CompanyApplication.java
+++ ihrm-company/src/main/java/com/ihrm/company/CompanyApplication.java
@@ -1,16 +1,25 @@
 package com.ihrm.company;
 
+import com.ihrm.common.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 // 1.配置springboot的包扫描
 @SpringBootApplication(scanBasePackages = "com.ihrm")
 // 2.配置jpa注解的扫描
 @EntityScan(value = "com.ihrm.domain.company")
 public class CompanyApplication {
-  public static void main(String[] args) {
-    /** 启动方法 */
-    SpringApplication.run(CompanyApplication.class, args);
-  }
+    public static void main(String[] args) {
+        /**
+         * 启动方法
+         * */
+        SpringApplication.run(CompanyApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
+    }
 }
---
+++ ihrm-company/src/main/java/com/ihrm/company/service/CompanyService.java
@@ -0,0 +1,50 @@
+package com.ihrm.company.service;
+
+import com.ihrm.common.utils.IdWorker;
+import com.ihrm.company.dao.CompanyDao;
+import com.ihrm.domain.company.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CompanyService {
+    @Autowired
+    private CompanyDao companyDao;
+
+    @Autowired
+    private IdWorker idWorker;
+
+    /**
+     * 保存用户 1.配置idwork到工程
+     * 2.在service中注入idwork
+     * 3.通过idwork生成id
+     * 4.保存用户
+     */
+    public Company add(Company company) {
+        company.setId(idWorker.nextId() + "");
+        company.setCreateTime(new Date());
+        company.setState(1); //启用
+        company.setAuditState("0"); //待审核
+        company.setBalance(0d);
+        return companyDao.save(company);
+    }
+
+    public Company update(Company company) {
+        return companyDao.save(company);
+    }
+
+    public Company findById(String id) {
+        return companyDao.findById(id).get();
+    }
+
+    public void deleteById(String id) {
+        companyDao.deleteById(id);
+    }
+
+    public List<Company> findAll() {
+        return companyDao.findAll();
+    }
+}
